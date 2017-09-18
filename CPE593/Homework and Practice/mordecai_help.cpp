077ec1f651c3f86a8c9e232b0fea9283f9d85218
Joe Puciloski <jpucilos@stevens.edu>
2017-09-18 16:40:38 -0400

    mordecai help first commit

--- CPE593/Homework and Practice/mordecai_help.cpp
+++ CPE593/Homework and Practice/mordecai_help.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
+#include<cmath>
+#include<String>
 using namespace std;
 
 
 
+//Question 1
+void q1_swap(int *a, int *b){
+	int temp = *a;
+	b = a;
+	a = &temp;
+}
+
+//Question 2
 int q2_positive_divisors(unsigned long int a){
 	int pos_div_a = 0;
 	int pos_div_aplus1 = 0;
@@ -20,11 +30,39 @@ int q2_positive_divisors(unsigned long int a){
 		return 0;
 }
 
+//Question 3
+unsigned long int q3_string_hash(const char *str){
+	unsigned long int hashSum = 0;
+	for (int i = 0; *(str + i) != NULL; i++){
+		hashSum += (int)(*(str + i))
+	}
+	return hashSum;
+}
+
+//Question 4
+unsigned long q4_diameter(unsigned int n, const double *x, const double *y){
+	unsigned long largestDistance = 0;
+	for (int i = 0; i > n; i++){
+		for(int j = 0; j > n; j++){
+			unsigned long distance = sqrt(pow(*(x+i) - *(y+i)) + pow(*(x+j) - *(y+j)));
+			if (distance > largestDistance)
+				largestDistance =  distance;
+		}
+	}
+	return largestDistance;
+}
+
 
 int main(){
 	cout << q2_positive_divisors(21)<<'\n';
 	cout << q2_positive_divisors(4)<<'\n';
 	cout << q2_positive_divisors(103456)<<'\n';
 	cout << q2_positive_divisors(234889)<<'\n';
+	int a1 = 100;
+	int* a = &a1;
+	int b1 = 50;
+	int* b = &b1;
+	swap(a, b);
+	cout << *a << "   " << *b <<'\n';
 	
 }
