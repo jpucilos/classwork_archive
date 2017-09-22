f491ed847390d11eee308af1446e1c87c777b8b2
Joe Puciloski <jpucilos@stevens.edu>
2017-09-21 20:45:12 -0400

    work on Homework3

--- CPE555/Homework3.c
+++ CPE555/Homework3.c
@@ -1,24 +1,25 @@
-#include <stdio.h>
+/*
+	CPE 555 Homework3
+	Author: Joseph Puciloski
+	Date: 9/28/17
 
-int main()
-{
-	printf("Enter a Year: ");
+*/
+
+#include <stdio.h>
+int main(){
 	int year = -1;
-	while(year != 0){
-		
+	while(year != 0){		
 		printf("Enter a Year: ");
 		scanf("%d", &year);
-		
 		if(year > 1896){
-			if (year >= 1994 && (year - 1994 % 4) == 0)
+			if (year >= 1994 && (year - 1994) % 4 == 0)
 				printf("Winter Olympics\n");
-			else if(year >= 1924 && year <= 1992 &&  (year - 1896 % 4) == 0)
+			else if(year >= 1924 && year <= 1992 && (year - 1896) % 4 == 0)
 				printf("Summer and winter Olympics\n");
-			else if(year < 1924 && (year -1896 % 4) == 0)
+			else if(year < 1924 && (year - 1896) % 4 == 0)
 				printf("Summer Olympics\n");
 		}
 		else
 			printf("No Olympics\n");
 	}
-	return 0;
 }
